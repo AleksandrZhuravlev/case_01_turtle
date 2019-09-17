e1dd3ba177f563a19f31173e60ecba3914958af5
AleksandrZhuravlev <mafanyazhura@gmail.com>
2019-09-17 14:11:31 +0700

    added todo

--- case_01_turtle/main.py
+++ case_01_turtle/main.py
@@ -3,5 +3,13 @@
 #               Starnovskiy Sergey (),
 #                Drachev Nikita().
 import turtle
-#todo:()
+def square(x,y,size,color,tilt)
+#todo:(zhuravlev aleksandr) make sqare drawing function
+pass
+def triangle_regular(x,y,size,color,tilt)
+#todo:(Starnovskiy Sergey) make triangle drawing function
+pass
+def circle(x,y,size,color)
+#todo:(Drachev Nikita) make circle drawing function
+pass
 
