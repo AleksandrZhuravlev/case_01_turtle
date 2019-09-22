d63e233901e61cf282d2670d4e7cf169119e69e6
AleksandrZhuravlev <mafanyazhura@gmail.com>
2019-09-22 16:25:31 +0700

    square function done

--- case_01_turtle/main.py
+++ case_01_turtle/main.py
@@ -3,10 +3,24 @@
 #               Starnovskiy Sergey (),
 #                Drachev Nikita().
 import turtle
-def square(x,y,size,color,tilt)
+def square(x, y, size, tilt):
+        # todo:(zhuravlev aleksandr) make sqare drawing function
+        turtle.penup()
+        turtle.goto(x, y)
+        turtle.pendown()
+        turtle.rt(tilt)
+        turtle.rt(90)
+        turtle.fd(size)
+        turtle.rt(90)
+        turtle.fd(size)
+        turtle.rt(90)
+        turtle.fd(size)
+        turtle.rt(90)
+        turtle.fd(size)
+        return (0)
 #todo:(zhuravlev aleksandr) make sqare drawing function
 pass
-def triangle_regular(x,y,size,color,tilt)
+def triangle_regular(x,y,size,color,tilt):
 #todo:(Starnovskiy Sergey) make triangle drawing function
 pass
 def Parallelogram(x,y,size,color)
