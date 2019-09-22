dcb0d23f8405eada09b30f0368d2a998e065135c
AleksandrZhuravlev <mafanyazhura@gmail.com>
2019-09-22 16:16:42 +0700

    square function done

--- case_01_turtle/main.py
+++ case_01_turtle/main.py
@@ -3,13 +3,26 @@
 #               Starnovskiy Sergey (),
 #                Drachev Nikita().
 import turtle
-def square(x,y,size,color,tilt)
+def square(x, y, size, tilt):
 #todo:(zhuravlev aleksandr) make sqare drawing function
-pass
-def triangle_regular(x,y,size,color,tilt)
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.pendown()
+    turtle.rt(tilt)
+    turtle.rt(90)
+    turtle.fd(size)
+    turtle.rt(90)
+    turtle.fd(size)
+    turtle.rt(90)
+    turtle.fd(size)
+    turtle.rt(90)
+    turtle.fd(size)
+    return (0)
+def triangle_regular(x, y, size, color, tilt):
 #todo:(Starnovskiy Sergey) make triangle drawing function
-pass
-def circle(x,y,size,color)
+    pass
+def circle(x, y, size, color):
 #todo:(Drachev Nikita) make circle drawing function
-pass
+    pass
+
 
