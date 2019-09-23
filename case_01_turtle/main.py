2b6cc8f4251811524c04a7e3be649e710661bc78
AleksandrZhuravlev <mafanyazhura@gmail.com>
2019-09-23 21:09:50 +0700

    man1 and man2 functions

--- case_01_turtle/main.py
+++ case_01_turtle/main.py
@@ -21,6 +21,7 @@ def square(x, y, size,color, tilt):
         turtle.rt(90)
         turtle.fd(size)
         turtle.end_fill()
+
         return (0)
 pass
 #todo:(zhuravlev aleksandr) make sqare drawing function
@@ -73,7 +74,17 @@ def man1():
     triangle_regular(-22,-80,50,'cyan',30)
     triangle_regular(-24,-180,40,'violet',255)
     triangle_regular(-130, -129, 40, 'pink', 120)
-print(man1())
+    turtle.rt(225)
+
+def man2():
+    square(170, 50, 40, 'yellow', 45)
+    triangle_regular(141,-55,70,'red',165)
+    triangle_regular(70, 15, 70, 'orange', 165)
+    Parallelogram(112,-122,40,'lightgreen',0)
+    triangle_regular(191,-102,50,'lightblue',345)
+    triangle_regular(175,-133,30,'violet',165)
+    triangle_regular(120,-115,30,'pink',255)
+
+man1()
+man2()
 time.sleep(3)
-
-
